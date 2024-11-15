4426c57bdde80f52101fe41c3f1dca3e8cf5c466
Brody Reid <brodyhreid@gmail.com>
2024-11-14 22:36:12 -0500

    /projects/versions/contributors query

--- src/index.js
+++ src/index.js
@@ -22,13 +22,14 @@ app.get('/projects', async (_req, res) => {
   }
 });
 
-app.get('/projects/contributors', async (_req, res) => {
+app.get('/projects/versions/contributors', async (_req, res) => {
   try {
     const result = await query(`
-      SELECT p.id, p.title, p.folder_path, p.notes, p.date_created, array_agg(c."name") as contributors
+      SELECT p.id, p.title, p.folder_path, p.notes, p.date_created, ARRAY_AGG(DISTINCT c."name") as contributors, ARRAY_AGG(DISTINCT v."name") as versions
       FROM projects p
-      JOIN project_contributors pc ON pc.project_id = p.id
-      JOIN contributors c ON c.id = pc.contributor_id
+      LEFT JOIN project_contributors pc ON pc.project_id = p.id
+      LEFT JOIN contributors c ON c.id = pc.contributor_id
+      LEFT JOIN versions v on v.project_id = p.id
       GROUP BY p.id
       ORDER BY p.id;
       `
